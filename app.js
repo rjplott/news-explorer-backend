603d203c60458c6a2c4a662cdc518c3ebae47cc0
Joe Lott <rjplott@gmail.com>
2022-02-12 17:30:51 -0500

    bug: add support for getting db address from .env

--- app.js
+++ app.js
@@ -22,14 +22,18 @@ function validateEmail(string, helpers) {
   return string;
 }
 
-mongoose.connect('mongodb://localhost:27017/news-explorer', {
+const { PORT = 3000, NODE_ENV, DB_ADDRESS } = process.env;
+
+const dbAddress = NODE_ENV === 'production'
+  ? DB_ADDRESS
+  : 'mongodb://localhost:27017/news-explorer';
+
+mongoose.connect(dbAddress, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
-
-const { PORT = 3000 } = process.env;
 
 app.use(helmet());
 app.use(cors());
