93145e9b1e8db600caa564a8f35e459d36038cb4
Joe Lott <rjplott@gmail.com>
2022-04-10 12:00:16 -0400

    bug: refactor articles route to remove repeated function logic

--- routes/articles.js
+++ routes/articles.js
@@ -1,53 +1,72 @@
 const { celebrate, Joi } = require('celebrate');
 const validator = require('validator');
 const articleRouter = require('express').Router();
-const { getArticles, createArticle, deleteArticle } = require('../controllers/articles');
+const {
+  getArticles,
+  createArticle,
+  deleteArticle,
+} = require('../controllers/articles');
 
-function validateLinkUrl(string, helpers) {
-  if (!validator.isURL(string)) {
-    return helpers.error(400, '"link" is not a valid URL');
-  }
-  return string;
+function createUrlValidator(errorMessage) {
+  return function validateUrl(string, helpers) {
+    if (!validator.isURL(string)) {
+      return helpers.error(400, errorMessage);
+    }
+    return string;
+  };
 }
-function validateImageUrl(string, helpers) {
-  if (!validator.isURL(string)) {
-    return helpers.error(400, '"image" is not a valid URL');
-  }
-  return string;
-}
 
-articleRouter.get('/articles', celebrate({
-  body: Joi.object().keys({
-    user: Joi.object().keys({
-      _id: Joi.string().required().hex().length(24),
-    }).unknown(true),
-  }),
-}), getArticles);
+const validateImageUrl = createUrlValidator('"image" is not a valid URL');
+const validateLinkUrl = createUrlValidator('"link" is not a valid URL');
 
-articleRouter.post('/articles', celebrate({
-  body: Joi.object().keys({
-    keyword: Joi.string().required(),
-    title: Joi.string().required(),
-    text: Joi.string().required(),
-    date: Joi.string().required(),
-    source: Joi.string().required(),
-    link: Joi.string().required().custom(validateLinkUrl),
-    image: Joi.string().required().custom(validateImageUrl),
-    user: Joi.object().keys({
-      _id: Joi.string().required().hex().length(24),
+articleRouter.get(
+  '/articles',
+  celebrate({
+    body: Joi.object().keys({
+      user: Joi.object()
+        .keys({
+          _id: Joi.string().required().hex().length(24),
+        })
+        .unknown(true),
     }),
   }),
-}), createArticle);
+  getArticles,
+);
 
-articleRouter.delete('/articles/:articleId', celebrate({
-  params: Joi.object().keys({
-    articleId: Joi.string().required().hex().length(24),
+articleRouter.post(
+  '/articles',
+  celebrate({
+    body: Joi.object().keys({
+      keyword: Joi.string().required(),
+      title: Joi.string().required(),
+      text: Joi.string().required(),
+      date: Joi.string().required(),
+      source: Joi.string().required(),
+      link: Joi.string().required().custom(validateLinkUrl),
+      image: Joi.string().required().custom(validateImageUrl),
+      user: Joi.object().keys({
+        _id: Joi.string().required().hex().length(24),
+      }),
+    }),
   }),
-  body: Joi.object().keys({
-    user: Joi.object().keys({
-      _id: Joi.string().required().hex().length(24),
-    }).unknown(true),
+  createArticle,
+);
+
+articleRouter.delete(
+  '/articles/:articleId',
+  celebrate({
+    params: Joi.object().keys({
+      articleId: Joi.string().required().hex().length(24),
+    }),
+    body: Joi.object().keys({
+      user: Joi.object()
+        .keys({
+          _id: Joi.string().required().hex().length(24),
+        })
+        .unknown(true),
+    }),
   }),
-}), deleteArticle);
+  deleteArticle,
+);
 
 module.exports = articleRouter;
